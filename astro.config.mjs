8174b9f325ae6097a6af7da39649a5e2f4c19da9
Filip Zachariassen <filzac98@hotmail.com>
2025-04-30 14:36:26 +0100

    test again again

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
-
-export default defineConfig({
-  output: "server", // Ensure output is set to 'server' for SSR
-  adapter: netlify(),
-});
